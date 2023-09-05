acc78fc795b6b63e55711a4a4fafe3ee8d587af6
valensixx <valensixx@gmail.com>
2023-09-05 15:43:28 +0300

    setting up the server

--- tutorial_03/server.js
+++ tutorial_03/server.js
@@ -1,8 +1,11 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+
 const logEvents = require('./logEvents');
-
 const EventEmitter = require('events');
-
-class MyEmitter extends EventEmitter { };
+class Emitter extends EventEmitter { };
 /*
 In JavaScript, a class is a blueprint or a template for creating objects. 
 It provides a convenient way to define and create objects with 
@@ -10,12 +13,23 @@ shared properties and methods.
 */
 
 //initialize object
-const myEmitter = new MyEmitter();
+const myEmitter = new Emitter();
+
+const PORT = process.env.PORT || 3500;
+
+//setting up our server:
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+}); //result: Server running on port 3500 // / GET
 
 //add listener for the log event
+/*
 myEmitter.on('log', (msg) => logEvents(msg));
 
-setTimeout(() => {
-    //Emit event
+
     myEmitter.emit('log', 'Log event emitted!');
-}, 2000);
+*/
