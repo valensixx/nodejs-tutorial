ce642a02d1c804a8f9b30e3a031f31be0fc7cd27
valensixx <valensixx@gmail.com>
2023-09-05 16:05:57 +0300

    working on server logics

--- tutorial_03/server.js
+++ tutorial_03/server.js
@@ -20,6 +20,68 @@ const PORT = process.env.PORT || 3500;
 //setting up our server:
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
+
+    const extension = path.extname(req.url);
+
+    let contentType;
+
+    switch (extension) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.jpg':
+            contentType = 'image/jpeg';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.txt':
+            contentType = 'text/plain';
+            break;
+        default:
+            contentType = 'text/html';
+    }
+
+    let filePath =
+        contentType === 'text/html' && req.url === '/'
+            ? path.join(__dirname, 'views', 'index.html')
+            : contentType === 'text/html' && req.url.slice(-1) === '/'
+                ? path.join(__dirname, 'views', req.url, 'index.html')
+                : contentType === 'text/html'
+                    ? path.join(__dirname, 'views', req.url)
+                    : path.join(__dirname, req.url);
+
+    //makes the .html extension not required in the browser
+    if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
+
+    const fileExists = fs.existsSync(filePath);
+
+    if (fileExists) {
+        //serve the file
+    } else {
+        //404
+        //301 redirect
+        console.log(path.parse(filePath));
+
+        //we got response:
+        /*
+            Server running on port 3500
+            / GET
+            {
+             root: 'C:\\',
+            dir: 'C:\\Users\\valen\\Desktop\\nodejs\\tutorial_03\\views',
+            base: 'index.html',
+            ext: '.html',
+            name: 'index'
+            }
+        */
+    }
 });
 
 server.listen(PORT, () => {
