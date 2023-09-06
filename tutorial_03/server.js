b8828c876f4c517c81699af7aa33cb3fd9071f44
valensixx <valensixx@gmail.com>
2023-09-06 13:28:43 +0300

    Route handlers and chaining done

--- tutorial_03/server.js
+++ tutorial_03/server.js
@@ -1,10 +1,52 @@
 const express = require('express');
-const http = require('http');
+const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3500;
 
+app.get('^/$|/index(.html)?', (req, res)=>{ // '^/$|/index.html' this says start with / and end with / or /index.html we made .index optional
+   // res.sendFile('./views/index.html', {root: __dirname}); //One way to do it 
+    res.sendFile(path.join(__dirname, 'views', 'index.html')); //Second way to do it
+});
 
+app.get('/new-page(.html)?', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get('/old-page(.html)?', (req, res)=>{
+    res.redirect(301, '/new-page.html'); // 302 by default
+});
+
+//Route handlers 
+app.get('/hello(.html)?', (req, res, next) => {
+    console.log('attempted to load hello.html');
+    next();
+}, (req, res)=>{
+    res.send('Hello World!');
+});
+
+//Chaining route handlers:
+const one = (req, res, next) => {
+    console.log('one');
+    next();
+}
+
+const two = (req, res, next) => {
+    console.log('two');
+    next();
+}
+
+const three = (req, res) => {
+    console.log('three');
+    res.send('Finished');
+}
+
+app.get('/chain(.html)?', [one, two, three]);
+
+app.get('/*', (req, res) => { //selet all *
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+}); 
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
 
