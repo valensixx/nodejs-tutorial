cb005989d886afba85b4cd3d9be7e044a944d9a1
valensixx <valensixx@gmail.com>
2023-09-06 14:19:21 +0300

    cors

--- tutorial_03/server.js
+++ tutorial_03/server.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
+const {logger} = require('./middleware/logEvents');
 const PORT = process.env.PORT || 3500;
 
 //custom middleware logger
-app.use((req, res, next)=>{
-    console.log(`${req.method} ${req.path}`);
-    next();
-});
+app.use(logger);
 
+//cors = Cross Origin Resource Sharing
+const whitelist = ['https://www.yoursite.com','http://127.0.0.1:5500','http://localhost:3500'];
+const corsOptions = {
+    origin:(origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1){
+            callback(null, true)
+        }else{
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 //built-in middleware to handle urlencoded data = form data:
 //`content-type: application/x-www-form-urlencoded` 
