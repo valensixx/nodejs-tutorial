5b465f4d9c81bbceda9d991d5de3bdf945451a74
valensixx <valensixx@gmail.com>
2023-09-06 15:02:44 +0300

    fixed final errors and done

--- tutorial_03/server.js
+++ tutorial_03/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
-const {logger} = require('./middleware/logEvents');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
 
 //custom middleware logger
@@ -12,7 +13,7 @@ app.use(logger);
 const whitelist = ['https://www.yoursite.com','http://127.0.0.1:5500','http://localhost:3500'];
 const corsOptions = {
     origin:(origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1){
+        if (whitelist.indexOf(origin) !== -1 || !origin){
             callback(null, true)
         }else{
             callback(new Error('Not allowed by CORS'));
@@ -71,9 +72,20 @@ const three = (req, res) => {
 
 app.get('/chain(.html)?', [one, two, three]);
 
-app.get('/*', (req, res) => { //selet all *
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-}); 
+
+//app use('/')
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ "error": "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
