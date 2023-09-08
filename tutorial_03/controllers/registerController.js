f0fa8549df38bbbd7ad363e88f060ae2b3dfb895
valensixx <valensixx@gmail.com>
2023-09-08 17:14:58 +0300

    working on registerController

--- tutorial_03/controllers/registerController.js
+++ tutorial_03/controllers/registerController.js
@@ -4,3 +4,17 @@ const userDB = {
 }
 const fsPromises =  require('fs').promises;
 const path = require('path');
+const bcrypt = require('bcrypt');
+
+const handleNewUser = async (req, res) => {
+    const {user, pwd} = req.body;
+    if(!user || !pwd) return res.status(400).json({'message':'Username and password are required.'});
+    //check for duplicate username in the db
+    const duplicate = usersDB.users.fing(person => person.username === user);
+    if(duplicate) return res.sendStatus(409); //conflict
+    try{
+
+    }catch(err){
+        
+    }
+}
