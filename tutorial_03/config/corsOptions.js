13f7716ff0e19f5d81310b90438dd1f244e7da7d
valensixx <valensixx@gmail.com>
2023-09-08 16:36:54 +0300

    controller done and deleted some files

---
+++ tutorial_03/config/corsOptions.js
@@ -0,0 +1,19 @@
+//cors = Cross Origin Resource Sharing
+const whitelist = [
+    'https://www.yoursite.com',
+    'http://127.0.0.1:5500',
+    'http://localhost:3500'
+];
+const corsOptions = {
+    origin:(origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin){
+            callback(null, true)
+        }else{
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
+module.exports = corsOptions;
--- tutorial_03/server.js
+++ tutorial_03/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
 const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
@@ -9,19 +10,7 @@ const PORT = process.env.PORT || 3500;
 //custom middleware logger
 app.use(logger);
 
-//cors = Cross Origin Resource Sharing
-const whitelist = ['https://www.yoursite.com','http://127.0.0.1:5500','http://localhost:3500'];
-const corsOptions = {
-    origin:(origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1 || !origin){
-            callback(null, true)
-        }else{
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    optionsSuccessStatus: 200
-}
-app.use(cors(corsOptions));
+
 
 //built-in middleware to handle urlencoded data = form data:
 //`content-type: application/x-www-form-urlencoded` 
@@ -32,11 +21,9 @@ app.use(express.json());
 
 //serve ststic files; This is still middlewere
 app.use('/', express.static(path.join(__dirname, '/public')));
-app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
 // routes
 app.use('/', require('./routes/root'));
-app.use('/subdir', require('./routes/subdir'));
 app.use('/employees', require('./routes/api/employees'));//now we will focus no REST API
 
 
