aae415c8413b5f69a960176edffafa01503ebbca
valensixx <valensixx@gmail.com>
2023-09-09 16:04:00 +0300

    working on JWT authentication

--- tutorial_03/controllers/authController.js
+++ tutorial_03/controllers/authController.js
@@ -4,6 +4,11 @@ const userDB = {
 }
 const bcrypt = require('bcrypt');
 
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const fsPromises = require('fs').promises;
+const path = require('path');
+
 const handleLogin = async (req, res) => {
     const {user, pwd} = req.body;
     if(!user || !pwd) return res.status(400).json({'message':'Username and password are required.'});
