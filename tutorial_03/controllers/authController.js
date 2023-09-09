24c4fa8ca077cf858b2ac4829715551c5c4e8ace
valensixx <valensixx@gmail.com>
2023-09-09 16:19:24 +0300

    jwt logics

--- tutorial_03/controllers/authController.js
+++ tutorial_03/controllers/authController.js
@@ -1,6 +1,6 @@
 const userDB = {
     users: require('../model/users.json'),
-    setUsers: function (data) {this.users = data}
+    setUsers: function (data) { this.users = data }
 }
 const bcrypt = require('bcrypt');
 
@@ -10,18 +10,37 @@ const fsPromises = require('fs').promises;
 const path = require('path');
 
 const handleLogin = async (req, res) => {
-    const {user, pwd} = req.body;
-    if(!user || !pwd) return res.status(400).json({'message':'Username and password are required.'});
+    const { user, pwd } = req.body;
+    if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
     const foundUser = usersDB.users.find(person => person.username === user);
-    if(!foundUser) return res.sendStatus(401); //Unduthorized
+    if (!foundUser) return res.sendStatus(401); //Unduthorized
     // evaluate password 
     const match = await bcrypt.compare(pwd, foundUser.password);
-    if(match){
+    if (match) {
         //create JWTs 
-        res.json({'success': `User ${user} is logged in!`});
-    }else{
+        const accessToken = jwt.sign(
+            { "username": foundUser.username },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '30s' }
+        );
+        const refreshToken = jwt.sign(
+            { "username": foundUser.username },
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: '1d' }
+        );
+        //Saving refreshToken with current user. 
+        const otherUsers = userDB.users.filter(person => person.username !== foundUser.username);
+        const currentUser = { ...foundUser, refreshToken };
+        usersDB.setUsers([...otherUsers, currentUser]);
+        await fsPromises.writeFile(
+            path.join(__dirname, '..', 'model', 'users.json'),
+            JSON.stringify(usersDB.users)
+        );
+        res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 }) // one day time
+        res.json({ accessToken });
+    } else {
         res.sendStatus(401);
     }
 }
 
-module.exports = {handleLogin};
+module.exports = { handleLogin };
