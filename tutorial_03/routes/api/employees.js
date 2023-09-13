565af1923d71130d16415b78e2a3b764944563b4
valensixx <valensixx@gmail.com>
2023-09-13 16:48:26 +0300

    fixed connection to MongoDB err

--- tutorial_03/routes/api/employees.js
+++ tutorial_03/routes/api/employees.js
@@ -7,8 +7,8 @@ const verifyRoles = require('../../middleware/verifyRoles');
 router.route('/')
     .get(employeesController.getAllEmployees)
     .post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), employeesController.createNewEmployee)
-    .put(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor),employeesController.updateEmployee)
-    .delete(verifyRoles(ROLES_LIST.Admin,),employeesController.deleteEmployee);
+    .put(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), employeesController.updateEmployee)
+    .delete(verifyRoles(ROLES_LIST.Admin), employeesController.deleteEmployee);
 
 router.route('/:id')
     .get(employeesController.getEmployee);
--- tutorial_03/server.js
+++ tutorial_03/server.js
@@ -9,11 +9,11 @@ const errorHandler = require('./middleware/errorHandler');
 const verifyJWT = require('./middleware/verifyJWT');
 const cookieParser = require('cookie-parser');
 const credentials = require('./middleware/credentials');
-const mongoose = require('mongoose'); 
+const mongoose = require('mongoose');
 const connectDB = require('./config/dbConn');
 const PORT = process.env.PORT || 3500;
 
-//Connect to MongoDB: 
+// Connect to MongoDB
 connectDB();
 
 // custom middleware logger
